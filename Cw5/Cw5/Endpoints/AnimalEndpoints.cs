5ad81147abc7f03adb0a47bba11cff12ebe3b9a5
Fiodar Piatrou <s22854@pjwstk.edu.pl>
2024-04-15 22:57:50 +0200

    MapDelete

--- Cw5/Cw5/Endpoints/AnimalEndpoints.cs
+++ Cw5/Cw5/Endpoints/AnimalEndpoints.cs
@@ -17,28 +17,40 @@ public static class AnimalEndpoints
             //201 - Created
             var animals = StaticData.Animals;
             return Results.Ok(animals);
-        } );
+        });
         app.MapGet("/animals/{id}", (int id) =>
         {
-            if (id>StaticData.Animals.Count-1)
+            if (id > StaticData.Animals.Count - 1)
             {
                 return Results.BadRequest("No Animal with given id");
             }
+
             return Results.Ok(StaticData.Animals[id]);
         });
         app.MapPost("/animals", (Animal animal) =>
         {
-            if (StaticData.Animals.Exists((animal1 => animal1.Id==animal.Id )))
+            if (StaticData.Animals.Exists((animal1 => animal1.Id == animal.Id)))
             {
                 return Results.BadRequest("Animal with a given id exists");
             }
+
             StaticData.Animals.Add(animal);
-            return Results.Created("",animal);
+            return Results.Created("", animal);
         });
         app.MapPut("/animals/{id}", (int id, Animal animal) =>
         {
             StaticData.Animals[id] = animal;
             return Results.Ok();
+        });
+        app.MapDelete("/animals/{id}", (int id) =>
+        {
+            if (StaticData.Animals.Exists(animal => animal.Id == id))
+            {
+                StaticData.Animals.Remove(StaticData.Animals[id]);
+                return Results.Ok("Deleted");
+            }
+
+            return Results.BadRequest("No animal with given Id");
         });
     }
 }
