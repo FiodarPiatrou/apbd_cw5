295e978d1532801cdba5ac5b498ed7e1b3f3ac7d
Fiodar Piatrou <s22854@pjwstk.edu.pl>
2024-04-15 22:47:47 +0200

    added 3 animals (my beloved Zyza included)

--- Cw5/Cw5/Database/StaticData.cs
+++ Cw5/Cw5/Database/StaticData.cs
@@ -6,9 +6,9 @@ public class StaticData
 {
     public static List<Animal> Animals = new List<Animal>()
     {
-        new Animal(),
-        new Animal(),
-        new Animal()
+        new Animal(){HairColor = "grey",Id = 0,Name = "Zyza",Type = "cat"},
+        new Animal(){HairColor = "orange",Id = 1,Name = "Garfield",Type = "cat"},
+        new Animal(){HairColor = "black",Id = 2,Name = "Beast of darkness",Type = "dog"}
 
     };
 }
